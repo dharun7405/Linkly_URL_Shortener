178f7b30dd2884ab477130a81c97b48a36d92541
dharun7405 <dharunsudha74@gmail.com>
2025-05-28 23:45:31 +0530

    Generating and Validating Jwt added

--- url-shortener-sb/src/main/java/com/url/url_shortener_sb/security/jwt/JwtUtils.java
+++ url-shortener-sb/src/main/java/com/url/url_shortener_sb/security/jwt/JwtUtils.java
@@ -1,8 +1,24 @@
 package com.url.url_shortener_sb.security.jwt;
 
+import com.url.url_shortener_sb.service.UserDetailsImpl;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    @Value("${jwt.expiration}")
+    private int jwtExpiration;
+
     //AUTHORIZATION -> BEARER <TOKEN>
     public String getJwtFromHeader(HttpServletRequest request){
         String bearerToken = request.getHeader("Authorization");
@@ -10,5 +26,39 @@ public class JwtUtils {
             return bearerToken.substring(7);
         }
         return  null;
+    }
+
+    public String generateToken(UserDetailsImpl userDetails){
+        String username = userDetails.getUsername();
+        String roles = userDetails.getAuthorities().stream()
+                .map(authority -> authority.getAuthority())
+                .collect(Collectors.joining(","));
+        return Jwts.builder()
+                .subject(username).claim("roles",roles)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date().getTime() + jwtExpiration )))
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUserNameFromJwtToken(String token){
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build().parseSignedClaims(token)
+                .getPayload().getSubject();
+    }
+
+    public boolean validateToken(String authToken){
+        try {
+            Jwts.parser().verifyWith((SecretKey) key())
+                    .build().parseSignedClaims(authToken);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
