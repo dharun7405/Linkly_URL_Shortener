c7a9cc8cd94080fbc118b522d5691d11dc28974a
dharun7405 <dharunsudha74@gmail.com>
2025-05-29 10:21:18 +0530

    userDetails Service Implemented

---
+++ url-shortener-sb/src/main/java/com/url/url_shortener_sb/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.url.url_shortener_sb.service;
+
+import com.url.url_shortener_sb.models.User;
+import com.url.url_shortener_sb.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with the username :"+username));
+        return UserDetailsImpl.build(user);
+    }
+}
---
+++ url-shortener-sb/src/main/java/com/url/url_shortener_sb/service/UserService.java
@@ -0,0 +1,4 @@
+package com.url.url_shortener_sb.service;
+
+public class UserService {
+}
