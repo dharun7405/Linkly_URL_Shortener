6fe82d9a62c8cea87bde925c3d22dd535a2f8ec5
dharun7405 <dharunsudha74@gmail.com>
2025-06-02 18:04:15 +0530

    Footer added

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -2,15 +2,19 @@ import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LandingPage from "./components/LandingPage.jsx";
 import AboutPage from "./components/AboutPage.jsx";
+import NavBar from "./components/NavBar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+          <NavBar/>
         <Routes>
           <Route path="/" element={<LandingPage/>} />
           <Route path="/about" element={<AboutPage/>} />
         </Routes>
+          <Footer/>
       </BrowserRouter>
     </>
   )
