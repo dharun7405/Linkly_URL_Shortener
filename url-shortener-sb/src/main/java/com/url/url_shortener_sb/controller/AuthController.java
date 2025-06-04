61c0e3f783f9810849f7aba77c20f92315b5a95c
dharun7405 <dharunsudha74@gmail.com>
2025-06-04 14:40:14 +0530

    CrossOrigin added

--- url-shortener-sb/src/main/java/com/url/url_shortener_sb/controller/AuthController.java
+++ url-shortener-sb/src/main/java/com/url/url_shortener_sb/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
+@CrossOrigin
 public class AuthController {
 
     private UserService userService;
--- url-shortener-sb/src/main/java/com/url/url_shortener_sb/controller/RedirectController.java
+++ url-shortener-sb/src/main/java/com/url/url_shortener_sb/controller/RedirectController.java
@@ -7,6 +7,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class RedirectController {
 
     @Autowired
