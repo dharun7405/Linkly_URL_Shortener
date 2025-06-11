bbc6100f73f67485b75f90c783f685816fc3812c
dharun7405 <dharunsudha74@gmail.com>
2025-06-11 17:31:02 +0530

    Redirection to Subdomains

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -1,30 +1,17 @@
 import './App.css'
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import LandingPage from "./components/LandingPage.jsx";
-import AboutPage from "./components/AboutPage.jsx";
-import NavBar from "./components/NavBar.jsx";
-import Footer from "./components/Footer.jsx";
-import RegisterPage from "./components/RegisterPage.jsx";
-import {Toaster} from "react-hot-toast";
-import LoginPage from "./components/LoginPage.jsx";
-import DashboardLayout from "./components/Dashboard/DashboardLayout.jsx";
+import {getApps} from "./utils/helper.js";
 
 function App() {
+
+    const CurrentApp = getApps();
+
   return (
-    <>
+
       <Router>
-          <NavBar/>
-          <Toaster position="bottom-center"/>
-        <Routes>
-            <Route path="/" element={<LandingPage/>} />
-            <Route path="/about" element={<AboutPage/>} />
-            <Route path="/register" element={<RegisterPage/>} />
-            <Route path="/login" element={<LoginPage/>} />
-            <Route path="/dashboard" element={<DashboardLayout/>} />
-        </Routes>
-          <Footer/>
+         <CurrentApp/>
       </Router>
-    </>
+
   )
 }
 
---
+++ url-shortener-frontend/src/AppRouter.jsx
@@ -0,0 +1,39 @@
+import NavBar from "./components/NavBar.jsx";
+import {Toaster} from "react-hot-toast";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import LandingPage from "./components/LandingPage.jsx";
+import AboutPage from "./components/AboutPage.jsx";
+import RegisterPage from "./components/RegisterPage.jsx";
+import LoginPage from "./components/LoginPage.jsx";
+import DashboardLayout from "./components/Dashboard/DashboardLayout.jsx";
+import Footer from "./components/Footer.jsx";
+import ShortenUrlPage from "./components/ShortenUrlPage.jsx";
+
+
+
+const AppRouter =()=>{
+
+    return (
+        <>
+            <NavBar/>
+            <Toaster position="bottom-center"/>
+            <Routes>
+                <Route path="/" element={<LandingPage/>} />
+                <Route path="/about" element={<AboutPage/>} />
+                <Route path="/register" element={<RegisterPage/>} />
+                <Route path="/login" element={<LoginPage/>} />
+                <Route path="/dashboard" element={<DashboardLayout/>} />
+            </Routes>
+            <Footer/>
+        </>
+    );
+}
+export default AppRouter;
+
+export const SubDomainRouter = () => {
+    return (
+        <Routes>
+            <Route path="/:url" element={<ShortenUrlPage />} />
+        </Routes>
+    )
+}
